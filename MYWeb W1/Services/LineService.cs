9006176236a3f633b6b3bd5b4e1e7f2b67a23766
umod <y35458680@gmail.com>
2024-05-06 17:07:50 +0800

    新增隨機給圖

--- MYWeb W1/Services/LineService.cs
+++ MYWeb W1/Services/LineService.cs
@@ -12,6 +12,16 @@ public class LineService : ILineService
     {
         var result = null as List<ISendMessage>;
 
+        string[] imageUrl = new string[] //擺圖片
+        {
+            "https://memeprod.sgp1.digitaloceanspaces.com/user-wtf/1714638857118.jpg",
+            "https://memeprod.sgp1.digitaloceanspaces.com/user-wtf/1714001627791.jpg",
+            "https://memeprod.sgp1.digitaloceanspaces.com/user-wtf/1713877267429.jpg",
+            "https://memeprod.sgp1.digitaloceanspaces.com/user-wtf/1713605845579.jpg",
+        };
+        Random rnd = new Random((int)DateTime.Now.TimeOfDay.TotalSeconds);
+        int index = rnd.Next(0, imageUrl.Length);
+
         result = new List<ISendMessage>
         {
             new TextMessage($"Receive a text event message \nchannelId={channelId}  \nuserId={userId}")
